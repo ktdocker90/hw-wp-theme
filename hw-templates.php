343ad7913fb97f7bfdde3f6b411eed67c394e331
Your Name <you@example.com>
2017-08-14 08:17:20 +0700

    change

--- hw-templates.php
+++ hw-templates.php
@@ -43,18 +43,18 @@ return array(
 	],
 	//'template_loop_product_title'=> '<h2 class="woocommerce-loop-product__title">%s</h2>',
 	//'template_loop_category_title'=> '<h2 class="woocommerce-loop-category__title">%s</h2>',	
-	'taxonomy_archive_description'=> '<div class="term-description">%s</div>',
-	'product_archive_description'=> '<div class="page-description">%s</div>',
+	'woo_taxonomy_archive_description'=> '<div class="term-description">%s</div>',
+	'woo_product_archive_description'=> '<div class="page-description">%s</div>',
 	
 	//move to woocommerce/custom/
 	//'widget_shopping_cart_button_view_cart'=> '<a href="%s" class="button wc-forward">%s</a>',
 	//'widget_shopping_cart_proceed_to_checkout'=> '<a href="%s" class="button checkout wc-forward">%s</a>',
 	
-	'subcategory_thumbnail'=> '<img src="%s" alt="%s" width="%s" height="%s" srcset="%s" sizes="%s" />',
+	'woo_subcategory_thumbnail'=> '<img src="%s" alt="%s" width="%s" height="%s" srcset="%s" sizes="%s" />',
 	//'single_variation'=> '<div class="woocommerce-variation single_variation"></div>',
 	//'sale_flash'=> '<span class="onsale">%s</span>',
 	
-	'breadcrumb_defaults'=> array(
+	'woo_breadcrumb_defaults'=> array(
 		'delimiter'   => '&nbsp;&#47;&nbsp;',
 		'wrap_before' => '<nav class="woocommerce-breadcrumb">',
 		'wrap_after'  => '</nav>',
@@ -62,41 +62,41 @@ return array(
 		'after'       => '',
 		'home'        => _x( 'Home', 'breadcrumb', 'woocommerce' ),
 	),
-	'breadrumb_home_url'=> 'http://vinacart.net',
+	'woo_breadrumb_home_url'=> 'http://vinacart.net',
 	'yoast_breadcrumb'=> [
 		'separator'=> '/'
 	],
-	'loop_shop_columns'=> array(
+	'woo_loop_shop_columns'=> array(
 		'default'=> '4',
 		'is_product_category'=> '4',
 		'is_product'=> '2',
 		'is_checkout'=> '4'
 	),
-	'content_before_cart_table'=> 'woo_before_cart_table',
-	'filter_product_categories_widget_args'=> 'filter_product_categories_widget_args',
+	'woo_content_before_cart_table'=> 'woo_before_cart_table',
+	'woo_filter_product_categories_widget_args'=> 'filter_product_categories_widget_args',
 	//accept array or function
-	'filter_product_tabs'=> function($tabs) {
+	'woo_filter_product_tabs'=> function($tabs) {
 		//unset( $tabs['description'] );
 		return $tabs;
 	},
-	'filter_page_title'=> null,
+	'woo_filter_page_title'=> null,
 	//'filter_default_catalog_orderby'=> 'date',
 	
-	'minimum_order_amount_error'=> 'You must have an order with a minimum of %s to place your order.',
-	//'action_thankyou'=> 'woo_email_order_coupons',
-	/*'filter_catalog_orderby'=> 'func',
-	'filter_product_add_to_cart_url'=> 'func',
-	'filter_get_price_html'=> 'func'
-	'filter_get_availability_class'=> 'func',
-	'filter_get_availability'=> 'func',
-	'filter_checkout_fields'=> '',
-	'filter_billing_fields'=> '',
-	'filter_shipping_fields'=> '',
-	'filter_default_address_fields'=> '',
-	'content_after_order_notes'=> '',*/
-	//'filter_upsell_display_args'=> '',
-	//'filter_output_related_products_args'=> '',
-	'filter_add_to_cart_fragments'=> 'filter_add_to_cart_fragments',
+	'woo_minimum_order_amount_error'=> 'You must have an order with a minimum of %s to place your order.',
+	//'woo_action_thankyou'=> 'woo_email_order_coupons',
+	/*'woo_filter_catalog_orderby'=> 'func',
+	'woo_filter_product_add_to_cart_url'=> 'func',
+	'woo_filter_get_price_html'=> 'func'
+	'woo_filter_get_availability_class'=> 'func',
+	'woo_filter_get_availability'=> 'func',
+	'woo_filter_checkout_fields'=> '',
+	'woo_filter_billing_fields'=> '',
+	'woo_filter_shipping_fields'=> '',
+	'woo_filter_default_address_fields'=> '',
+	'woo_content_after_order_notes'=> '',*/
+	//'woo_filter_upsell_display_args'=> '',
+	//'woo_filter_output_related_products_args'=> '',
+	'woo_filter_add_to_cart_fragments'=> 'filter_add_to_cart_fragments',
 	'navmenu'=> [
 		'primary'=> [
 			'ex_separator'=> '|',
@@ -124,21 +124,13 @@ return array(
 		]
 		
 	],
-	'checkout_fields'=> [
+	'woo_checkout_fields'=> [
 		'order_comments_input_class'=> ['form-control']
 	],
-	'address_fields'=> [
+	'woo_address_fields'=> [
 		'input_class'=> ['form-control'],
 		'label_class'=> ['control-label'],
 		'form_row_class'=> ['row1']
 	],
-	'sidebars'=> [
-		'sidebar-footer'=> [
-			'before_widget'=> '<div class="%2$s *1 col-md-2" id="%1$s">',
-			'before_title'=> '<h3 class="%2$s" style="{css_title};">',
-			'after_title'=> '</h3><div class="infomation">',
-			'after_widget'=> '</div></div>',
-			'description'=> '',
-		]
-	]
+	
 );
--- inc/framework/loader.php
+++ inc/framework/loader.php
@@ -2,7 +2,7 @@
 require_once (__DIR__. '/functions.php');
 
 require_once (__DIR__. '/class/class-core-validation.php');
-require_once (__DIR__. '/class/class-URL.php');
+require_once (__DIR__. '/class/class-url.php');
 require_once (__DIR__. '/class/class-core-string.php');
 require_once (__DIR__. '/class/class-core-directories.php');
 require_once (__DIR__. '/class/class-core-xml.php');
